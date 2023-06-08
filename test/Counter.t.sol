b53b7b23fbbbaee9c6ee5913b8b2786889fe7c62
Colin Platt <colingplatt@gmail.com>
2023-06-08 15:27:05 +0200

    fixing compiling errors

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,24 +1,9 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
-import "../src/Counter.sol";
+import "lib/forge-std/src/Test.sol";
+
 
 contract CounterTest is Test {
-    Counter public counter;
 
-    function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
-    }
-
-    function testIncrement() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testSetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
-    }
 }
